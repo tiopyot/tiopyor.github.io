6f521a373e8f0b2fa4566a479341646a2f306898
tiopyot <kattt2002@gmail.com>
2025-02-10 16:12:23 +0700

    10.02

---
+++ src/scripts/dev/main.js
@@ -0,0 +1,36 @@
+(function () {
+    "use strict";
+    const root = document.documentElement;
+    const navToggle = document.querySelector("#js-navToggle");
+    navToggle.addEventListener("click", function () {
+    root.classList.toggle("show-nav");
+    });   
+    const eventPP = document.querySelector("#js-eventPP");
+    if (eventPP) {
+       const eventOpenBtn = document.querySelector("#js-eventOpenBtn");
+       const closeEventPP = function (event) {
+         function close() {
+           document.removeEventListener("keyup", closeEventPP);
+           eventPP.removeEventListener("click", closeEventPP);
+           root.classList.remove("show-event-popup");
+         }
+         switch (event.type) {
+           case "keyup":
+             if (event.key === "Escape" || event.keyCode === 27) close();
+             break;
+           case "click":
+             if (
+               event.target === this ||
+               event.target.classList.contains("js-ppCloseBtn")
+             )
+               close();
+             break;
+         }
+       };
+       eventOpenBtn.addEventListener("click", function () {
+         root.classList.add("show-event-popup");
+         document.addEventListener("keyup", closeEventPP);
+         eventPP.addEventListener("click", closeEventPP);
+       });
+     }
+  })();
